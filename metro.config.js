72fc0a7e34d04eaacdc7aadfe8e6e2237fd637cf
xiaochanghai <xiaochanghai@foxmail.com>
2025-03-31 22:38:45 +0800

    feat: 🚀android build config

--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,22 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.blockList = [
+  /\/node_modules\/(?!react-native-css-interop\/.cache).*\/__tests__\//,
+];
+
+// 添加SVG转换器配置
+// 保留Expo默认transformer配置
+const defaultTransformer = getDefaultConfig(__dirname).transformer;
+
+config.transformer = {
+  ...defaultTransformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+};
+
+config.resolver = {
+  ...config.resolver,
+  sourceExts: [...config.resolver.sourceExts, 'svg'],
+};
+
 module.exports = withNativeWind(config, { input: './global.css' });
