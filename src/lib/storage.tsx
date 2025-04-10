86619bd8170534364e5535d818413591345669d5
xiaochanghai <xiaochanghai@foxmail.com>
2025-04-10 11:29:57 +0800

    feat: 🚀optimize storage.tsx

--- src/lib/storage.tsx
+++ src/lib/storage.tsx
@@ -2,9 +2,9 @@ import { MMKV } from 'react-native-mmkv';
 
 export const storage = new MMKV();
 
-export function getItem<T>(key: string): T {
+export function getItem<T>(key: string): T | null {
   const value = storage.getString(key);
-  return value ? JSON.parse(value) || null : null;
+  return value ? JSON.parse(value) : null;
 }
 
 export async function setItem<T>(key: string, value: T) {
